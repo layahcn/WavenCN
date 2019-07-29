e8c3afd5930760dc17d017b9dd199c5415bce881
layah <lyh1993@126.com>
2019-07-29 23:57:04 +0800

    最小化

--- WavenLauncher/WavenLauncher.Designer.vb
+++ WavenLauncher/WavenLauncher.Designer.vb
@@ -22,64 +22,81 @@ Partial Class WavenLauncher
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WavenLauncher))
-        Me.DownloadAL = New System.Windows.Forms.Button()
-        Me.LocAL = New System.Windows.Forms.Button()
-        Me.LocGame = New System.Windows.Forms.Button()
+        Me.StartAL = New System.Windows.Forms.Button()
+        Me.StartGame = New System.Windows.Forms.Button()
         Me.WLVersion = New System.Windows.Forms.Label()
         Me.ALVersion = New System.Windows.Forms.Label()
         Me.QuitForm = New System.Windows.Forms.Button()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
         Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.FormTitle = New System.Windows.Forms.Label()
+        Me.IcoPicture = New System.Windows.Forms.PictureBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.OpenSettings = New System.Windows.Forms.Button()
+        Me.SettingPanel = New System.Windows.Forms.Panel()
+        Me.LocGameCheck = New System.Windows.Forms.CheckBox()
+        Me.LocALCheck = New System.Windows.Forms.CheckBox()
+        Me.CloseAction = New System.Windows.Forms.GroupBox()
+        Me.CloseForm2 = New System.Windows.Forms.RadioButton()
+        Me.CloseForm1 = New System.Windows.Forms.RadioButton()
+        Me.CloseForm0 = New System.Windows.Forms.RadioButton()
+        Me.VersionState = New System.Windows.Forms.Label()
+        Me.SysTrayIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        CType(Me.IcoPicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SettingPanel.SuspendLayout()
+        Me.CloseAction.SuspendLayout()
         Me.SuspendLayout()
         '
-        'DownloadAL
+        'StartAL
         '
-        Me.DownloadAL.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.DownloadAL.Location = New System.Drawing.Point(321, 339)
-        Me.DownloadAL.Name = "DownloadAL"
-        Me.DownloadAL.Size = New System.Drawing.Size(117, 60)
-        Me.DownloadAL.TabIndex = 1
-        Me.DownloadAL.Text = "下载Ankama Launcher"
-        Me.DownloadAL.UseVisualStyleBackColor = True
+        Me.StartAL.BackColor = System.Drawing.Color.Teal
+        Me.StartAL.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.StartAL.Font = New System.Drawing.Font("锐字锐线怒放黑简1.0", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.StartAL.ForeColor = System.Drawing.Color.Yellow
+        Me.StartAL.Location = New System.Drawing.Point(449, 350)
+        Me.StartAL.Name = "StartAL"
+        Me.StartAL.Size = New System.Drawing.Size(115, 45)
+        Me.StartAL.TabIndex = 1
+        Me.StartAL.Text = "启动战网"
+        Me.StartAL.UseVisualStyleBackColor = False
         '
-        'LocAL
+        'StartGame
         '
-        Me.LocAL.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.LocAL.Location = New System.Drawing.Point(444, 339)
-        Me.LocAL.Name = "LocAL"
-        Me.LocAL.Size = New System.Drawing.Size(117, 60)
-        Me.LocAL.TabIndex = 2
-        Me.LocAL.Text = "汉化启动战网"
-        Me.LocAL.UseVisualStyleBackColor = True
-        '
-        'LocGame
-        '
-        Me.LocGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.LocGame.Location = New System.Drawing.Point(567, 339)
-        Me.LocGame.Name = "LocGame"
-        Me.LocGame.Size = New System.Drawing.Size(117, 60)
-        Me.LocGame.TabIndex = 3
-        Me.LocGame.Text = "汉化开始游戏"
-        Me.LocGame.UseVisualStyleBackColor = True
+        Me.StartGame.BackColor = System.Drawing.Color.Teal
+        Me.StartGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.StartGame.Font = New System.Drawing.Font("锐字锐线怒放黑简1.0", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.StartGame.ForeColor = System.Drawing.Color.Yellow
+        Me.StartGame.Location = New System.Drawing.Point(569, 350)
+        Me.StartGame.Name = "StartGame"
+        Me.StartGame.Size = New System.Drawing.Size(115, 45)
+        Me.StartGame.TabIndex = 2
+        Me.StartGame.Text = "开始游戏"
+        Me.StartGame.UseVisualStyleBackColor = False
         '
         'WLVersion
         '
         Me.WLVersion.AutoSize = True
-        Me.WLVersion.Location = New System.Drawing.Point(293, 17)
+        Me.WLVersion.BackColor = System.Drawing.Color.Transparent
+        Me.WLVersion.Font = New System.Drawing.Font("锐字锐线怒放黑简1.0", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.WLVersion.ForeColor = System.Drawing.Color.Yellow
+        Me.WLVersion.Location = New System.Drawing.Point(281, 19)
         Me.WLVersion.Name = "WLVersion"
-        Me.WLVersion.Size = New System.Drawing.Size(191, 12)
+        Me.WLVersion.Size = New System.Drawing.Size(63, 15)
         Me.WLVersion.TabIndex = 3
-        Me.WLVersion.Text = "汉化启动器版本：xxxx - by layah"
+        Me.WLVersion.Text = "版本号："
         '
         'ALVersion
         '
         Me.ALVersion.AutoSize = True
-        Me.ALVersion.Location = New System.Drawing.Point(12, 352)
+        Me.ALVersion.BackColor = System.Drawing.Color.Transparent
+        Me.ALVersion.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ALVersion.ForeColor = System.Drawing.Color.Yellow
+        Me.ALVersion.Location = New System.Drawing.Point(22, 350)
         Me.ALVersion.Name = "ALVersion"
-        Me.ALVersion.Size = New System.Drawing.Size(155, 12)
+        Me.ALVersion.Size = New System.Drawing.Size(205, 19)
         Me.ALVersion.TabIndex = 4
         Me.ALVersion.Text = "适用Ankama Launcher版本："
         '
@@ -88,26 +105,22 @@ Partial Class WavenLauncher
         Me.QuitForm.BackColor = System.Drawing.Color.Red
         Me.QuitForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.QuitForm.Font = New System.Drawing.Font("锐字锐线怒放黑简1.0", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.QuitForm.Location = New System.Drawing.Point(667, 2)
+        Me.QuitForm.Location = New System.Drawing.Point(659, 10)
         Me.QuitForm.Name = "QuitForm"
         Me.QuitForm.Size = New System.Drawing.Size(27, 27)
         Me.QuitForm.TabIndex = 0
         Me.QuitForm.Text = "×"
         Me.QuitForm.UseVisualStyleBackColor = False
         '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(173, 349)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(134, 21)
-        Me.TextBox2.TabIndex = 6
-        '
         'LinkLabel1
         '
         Me.LinkLabel1.AutoSize = True
-        Me.LinkLabel1.Location = New System.Drawing.Point(21, 381)
+        Me.LinkLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.LinkLabel1.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.Yellow
+        Me.LinkLabel1.Location = New System.Drawing.Point(23, 376)
         Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(71, 12)
+        Me.LinkLabel1.Size = New System.Drawing.Size(75, 17)
         Me.LinkLabel1.TabIndex = 7
         Me.LinkLabel1.TabStop = True
         Me.LinkLabel1.Text = "广告位招租1"
@@ -115,9 +128,12 @@ Partial Class WavenLauncher
         'LinkLabel2
         '
         Me.LinkLabel2.AutoSize = True
-        Me.LinkLabel2.Location = New System.Drawing.Point(123, 381)
+        Me.LinkLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.LinkLabel2.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.LinkLabel2.LinkColor = System.Drawing.Color.Yellow
+        Me.LinkLabel2.Location = New System.Drawing.Point(125, 376)
         Me.LinkLabel2.Name = "LinkLabel2"
-        Me.LinkLabel2.Size = New System.Drawing.Size(71, 12)
+        Me.LinkLabel2.Size = New System.Drawing.Size(75, 17)
         Me.LinkLabel2.TabIndex = 8
         Me.LinkLabel2.TabStop = True
         Me.LinkLabel2.Text = "广告位招租2"
@@ -125,12 +141,160 @@ Partial Class WavenLauncher
         'LinkLabel3
         '
         Me.LinkLabel3.AutoSize = True
-        Me.LinkLabel3.Location = New System.Drawing.Point(220, 381)
+        Me.LinkLabel3.BackColor = System.Drawing.Color.Transparent
+        Me.LinkLabel3.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.LinkLabel3.LinkColor = System.Drawing.Color.Yellow
+        Me.LinkLabel3.Location = New System.Drawing.Point(222, 376)
         Me.LinkLabel3.Name = "LinkLabel3"
-        Me.LinkLabel3.Size = New System.Drawing.Size(71, 12)
+        Me.LinkLabel3.Size = New System.Drawing.Size(75, 17)
         Me.LinkLabel3.TabIndex = 9
         Me.LinkLabel3.TabStop = True
         Me.LinkLabel3.Text = "广告位招租3"
+        '
+        'FormTitle
+        '
+        Me.FormTitle.AutoSize = True
+        Me.FormTitle.BackColor = System.Drawing.Color.Transparent
+        Me.FormTitle.Font = New System.Drawing.Font("锐字锐线怒放黑简1.0", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.FormTitle.ForeColor = System.Drawing.Color.Yellow
+        Me.FormTitle.Location = New System.Drawing.Point(45, 17)
+        Me.FormTitle.Name = "FormTitle"
+        Me.FormTitle.Size = New System.Drawing.Size(230, 17)
+        Me.FormTitle.TabIndex = 10
+        Me.FormTitle.Text = "Waven汉化启动器 by layah"
+        '
+        'IcoPicture
+        '
+        Me.IcoPicture.BackColor = System.Drawing.Color.Transparent
+        Me.IcoPicture.Image = CType(resources.GetObject("IcoPicture.Image"), System.Drawing.Image)
+        Me.IcoPicture.Location = New System.Drawing.Point(10, 10)
+        Me.IcoPicture.Name = "IcoPicture"
+        Me.IcoPicture.Size = New System.Drawing.Size(30, 30)
+        Me.IcoPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.IcoPicture.TabIndex = 11
+        Me.IcoPicture.TabStop = False
+        '
+        'ToolTip1
+        '
+        Me.ToolTip1.AutoPopDelay = 5000
+        Me.ToolTip1.InitialDelay = 100
+        Me.ToolTip1.IsBalloon = True
+        Me.ToolTip1.ReshowDelay = 100
+        '
+        'OpenSettings
+        '
+        Me.OpenSettings.BackColor = System.Drawing.Color.Teal
+        Me.OpenSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpenSettings.Font = New System.Drawing.Font("锐字锐线怒放黑简1.0", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.OpenSettings.ForeColor = System.Drawing.Color.Yellow
+        Me.OpenSettings.Location = New System.Drawing.Point(328, 350)
+        Me.OpenSettings.Name = "OpenSettings"
+        Me.OpenSettings.Size = New System.Drawing.Size(115, 45)
+        Me.OpenSettings.TabIndex = 12
+        Me.OpenSettings.Text = "打开设置"
+        Me.OpenSettings.UseVisualStyleBackColor = False
+        '
+        'SettingPanel
+        '
+        Me.SettingPanel.BackColor = System.Drawing.Color.Teal
+        Me.SettingPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.SettingPanel.Controls.Add(Me.LocGameCheck)
+        Me.SettingPanel.Controls.Add(Me.LocALCheck)
+        Me.SettingPanel.Controls.Add(Me.CloseAction)
+        Me.SettingPanel.ForeColor = System.Drawing.Color.Yellow
+        Me.SettingPanel.Location = New System.Drawing.Point(328, 229)
+        Me.SettingPanel.Name = "SettingPanel"
+        Me.SettingPanel.Size = New System.Drawing.Size(356, 115)
+        Me.SettingPanel.TabIndex = 13
+        Me.SettingPanel.Visible = False
+        '
+        'LocGameCheck
+        '
+        Me.LocGameCheck.AutoSize = True
+        Me.LocGameCheck.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.LocGameCheck.ForeColor = System.Drawing.Color.Yellow
+        Me.LocGameCheck.Location = New System.Drawing.Point(203, 80)
+        Me.LocGameCheck.Name = "LocGameCheck"
+        Me.LocGameCheck.Size = New System.Drawing.Size(99, 21)
+        Me.LocGameCheck.TabIndex = 2
+        Me.LocGameCheck.Text = "汉化启动游戏"
+        Me.LocGameCheck.UseVisualStyleBackColor = True
+        '
+        'LocALCheck
+        '
+        Me.LocALCheck.AutoSize = True
+        Me.LocALCheck.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.LocALCheck.ForeColor = System.Drawing.Color.Yellow
+        Me.LocALCheck.Location = New System.Drawing.Point(41, 80)
+        Me.LocALCheck.Name = "LocALCheck"
+        Me.LocALCheck.Size = New System.Drawing.Size(99, 21)
+        Me.LocALCheck.TabIndex = 1
+        Me.LocALCheck.Text = "汉化启动战网"
+        Me.LocALCheck.UseVisualStyleBackColor = True
+        '
+        'CloseAction
+        '
+        Me.CloseAction.Controls.Add(Me.CloseForm2)
+        Me.CloseAction.Controls.Add(Me.CloseForm1)
+        Me.CloseAction.Controls.Add(Me.CloseForm0)
+        Me.CloseAction.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.CloseAction.ForeColor = System.Drawing.Color.Yellow
+        Me.CloseAction.Location = New System.Drawing.Point(23, 20)
+        Me.CloseAction.Name = "CloseAction"
+        Me.CloseAction.Size = New System.Drawing.Size(310, 52)
+        Me.CloseAction.TabIndex = 0
+        Me.CloseAction.TabStop = False
+        Me.CloseAction.Text = "点击右上角关闭按钮时"
+        '
+        'CloseForm2
+        '
+        Me.CloseForm2.AutoSize = True
+        Me.CloseForm2.Location = New System.Drawing.Point(176, 23)
+        Me.CloseForm2.Name = "CloseForm2"
+        Me.CloseForm2.Size = New System.Drawing.Size(122, 21)
+        Me.CloseForm2.TabIndex = 2
+        Me.CloseForm2.TabStop = True
+        Me.CloseForm2.Text = "最小化到系统托盘"
+        Me.CloseForm2.UseVisualStyleBackColor = True
+        '
+        'CloseForm1
+        '
+        Me.CloseForm1.AutoSize = True
+        Me.CloseForm1.Location = New System.Drawing.Point(99, 23)
+        Me.CloseForm1.Name = "CloseForm1"
+        Me.CloseForm1.Size = New System.Drawing.Size(62, 21)
+        Me.CloseForm1.TabIndex = 1
+        Me.CloseForm1.TabStop = True
+        Me.CloseForm1.Text = "最小化"
+        Me.CloseForm1.UseVisualStyleBackColor = True
+        '
+        'CloseForm0
+        '
+        Me.CloseForm0.AutoSize = True
+        Me.CloseForm0.Location = New System.Drawing.Point(14, 23)
+        Me.CloseForm0.Name = "CloseForm0"
+        Me.CloseForm0.Size = New System.Drawing.Size(74, 21)
+        Me.CloseForm0.TabIndex = 0
+        Me.CloseForm0.TabStop = True
+        Me.CloseForm0.Text = "退出程序"
+        Me.CloseForm0.UseVisualStyleBackColor = True
+        '
+        'VersionState
+        '
+        Me.VersionState.AutoSize = True
+        Me.VersionState.BackColor = System.Drawing.Color.Transparent
+        Me.VersionState.Font = New System.Drawing.Font("锐字锐线怒放黑简1.0", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.VersionState.ForeColor = System.Drawing.Color.Yellow
+        Me.VersionState.Location = New System.Drawing.Point(446, 19)
+        Me.VersionState.Name = "VersionState"
+        Me.VersionState.Size = New System.Drawing.Size(0, 15)
+        Me.VersionState.TabIndex = 14
+        '
+        'SysTrayIcon
+        '
+        Me.SysTrayIcon.Icon = CType(resources.GetObject("SysTrayIcon.Icon"), System.Drawing.Icon)
+        Me.SysTrayIcon.Text = "NotifyIcon1"
+        Me.SysTrayIcon.Visible = True
         '
         'WavenLauncher
         '
@@ -138,34 +302,52 @@ Partial Class WavenLauncher
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(696, 411)
+        Me.Controls.Add(Me.VersionState)
+        Me.Controls.Add(Me.SettingPanel)
+        Me.Controls.Add(Me.OpenSettings)
+        Me.Controls.Add(Me.IcoPicture)
+        Me.Controls.Add(Me.FormTitle)
         Me.Controls.Add(Me.LinkLabel3)
         Me.Controls.Add(Me.LinkLabel2)
         Me.Controls.Add(Me.LinkLabel1)
-        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.QuitForm)
         Me.Controls.Add(Me.ALVersion)
         Me.Controls.Add(Me.WLVersion)
-        Me.Controls.Add(Me.LocGame)
-        Me.Controls.Add(Me.LocAL)
-        Me.Controls.Add(Me.DownloadAL)
+        Me.Controls.Add(Me.StartGame)
+        Me.Controls.Add(Me.StartAL)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "WavenLauncher"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Waven汉化启动器 by layah"
+        CType(Me.IcoPicture, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SettingPanel.ResumeLayout(False)
+        Me.SettingPanel.PerformLayout()
+        Me.CloseAction.ResumeLayout(False)
+        Me.CloseAction.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents DownloadAL As Button
-    Friend WithEvents LocAL As Button
-    Friend WithEvents LocGame As Button
+    Friend WithEvents StartAL As Button
+    Friend WithEvents StartGame As Button
     Friend WithEvents WLVersion As Label
     Friend WithEvents ALVersion As Label
     Friend WithEvents QuitForm As Button
-    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents LinkLabel1 As LinkLabel
     Friend WithEvents LinkLabel2 As LinkLabel
     Friend WithEvents LinkLabel3 As LinkLabel
+    Friend WithEvents FormTitle As Label
+    Friend WithEvents IcoPicture As PictureBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents OpenSettings As Button
+    Friend WithEvents SettingPanel As Panel
+    Friend WithEvents LocGameCheck As CheckBox
+    Friend WithEvents LocALCheck As CheckBox
+    Friend WithEvents CloseAction As GroupBox
+    Friend WithEvents CloseForm2 As RadioButton
+    Friend WithEvents CloseForm1 As RadioButton
+    Friend WithEvents CloseForm0 As RadioButton
+    Friend WithEvents VersionState As Label
+    Friend WithEvents SysTrayIcon As NotifyIcon
 End Class
--- WavenLauncher/WavenLauncher.vb
+++ WavenLauncher/WavenLauncher.vb
@@ -1,40 +1,69 @@
-﻿Public Class WavenLauncher
-    Const Version As UInt32 = 20190729  ' 汉化启动器版本号
-    Private bFormDragging As Boolean = False    ' 判断窗体是否被拖动
-    Private oPointClicked As Point  ' 记录鼠标拖动位置
+﻿Imports System.ComponentModel
+Imports System.Linq
+Public Class WavenLauncher
+    Const VersionWL As UInt32 = 20190729  ' 汉化启动器版本号
+    Const VersionAL As String = "2.9.20"  ' 适用战网版本号
+    Dim bFormDragging As Boolean = False    ' 判断窗体是否被拖动
+    Dim oPointClicked As Point  ' 记录鼠标拖动位置
+    Dim PanelVisible As Boolean = False   ' 设置界面默认隐藏
+    Dim CloseForm As UShort = 0
+    Dim LocAL As Boolean = True
+    Dim LocGame As Boolean = True   '预设用户设置条目
 
     Private Sub WavenLauncher_Load(sender As Object, e As EventArgs) Handles Me.Load
         ' 窗体载入时的动作
         Try
+            ToolTip1.SetToolTip(ALVersion, "点击下载Ankama Launcher官方客户端")
+            WLVersion.Text = WLVersion.Text _
+                             & VersionWL
+            ALVersion.Text = ALVersion.Text _
+                             & VersionAL  ' 显示版本号
+            CloseForm = My.Settings.CloseForm
+            LocAL = My.Settings.LocAL
+            LocGame = My.Settings.LocGame  ' 获取用户设置
             CheckVersion()
+            ' 调用检查版本函数
         Catch ex As Exception
-            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Check Version Error")
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Load Form Error")
         End Try
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles QuitForm.Click
-        ' 右上角退出程序叉叉
-        Application.Exit()
+        Try   ' 右上角退出程序叉叉
+            Select Case CloseForm
+                Case 0
+                    Close()
+                Case 1
+                    WindowState = FormWindowState.Minimized
+                Case 2
+                    Hide()
+                    SysTrayIcon.Visible = True
+                    SysTrayIcon.ShowBalloonTip(3000, "我在这儿", "我在这儿", ToolTipIcon.Info)
+            End Select
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Close Form Button Error")
+        End Try
     End Sub
 
 
     Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles Me.MouseDown
-        Me.bFormDragging = True
-        Me.oPointClicked = New Point(e.X, e.Y)
+        bFormDragging = True
+        oPointClicked = New Point(e.X, e.Y)
     End Sub
     Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp
-        Me.bFormDragging = False
+        bFormDragging = False
     End Sub
     Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
         Try
-            If Me.bFormDragging Then
+            If bFormDragging Then
                 Dim oMoveToPoint As Point
                 ' 以当前鼠标位置为基础，找出目标位置
-                oMoveToPoint = Me.PointToScreen(New Point(e.X, e.Y))
+                oMoveToPoint = PointToScreen(New Point(e.X, e.Y))
                 ' 根据开始位置作出调整
-                oMoveToPoint.Offset(Me.oPointClicked.X * -1, Me.oPointClicked.Y * -1)
+                oMoveToPoint.Offset(oPointClicked.X * -1, oPointClicked.Y * -1)
                 ' 移动窗体
-                Me.Location = oMoveToPoint
+                Location = oMoveToPoint
             End If
         Catch ex As Exception
             MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Move Form Error")
@@ -45,7 +74,7 @@
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles DownloadAL.Click
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles ALVersion.Click
         ' 直接调用浏览器下载Ankama Launcher
         Try
             Process.Start("https://ankama.akamaized.net/zaap/installers/production/Ankama%20Launcher-Setup.exe")
@@ -54,13 +83,81 @@
         End Try
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles LocGame.Click
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles StartGame.Click
         MsgBox("咕咕咕，在做了")
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles LocAL.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles StartAL.Click
         ' 汉化Ankama Launcher
+        Try
 
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Localisation Error")
+        End Try
     End Sub
 
+    Private Sub Settings_Click(sender As Object, e As EventArgs) Handles OpenSettings.Click
+        Try
+            If PanelVisible = False Then
+                SettingPanel.Visible = True
+                OpenSettings.Text = "保存设置"
+                PanelVisible = True
+                Try   '读取设置显示在设置界面
+                    Select Case CloseForm
+                        Case 0
+                            CloseForm0.Checked = True
+                        Case 1
+                            CloseForm1.Checked = True
+                        Case 2
+                            CloseForm2.Checked = True
+                    End Select
+                    If LocAL = True Then
+                        LocALCheck.Checked = True
+                    End If
+                    If LocGame = True Then
+                        LocGameCheck.Checked = True
+                    End If
+                Catch ex As Exception
+                    MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Get CloseFormAction Error")
+                    CloseForm0.Checked = True
+                End Try
+            Else
+                Try
+                    Dim rButton As RadioButton =   '判断GroupBox控件中是哪一个单选按钮被选中
+                            CloseAction.Controls _
+                            .OfType(Of RadioButton) _
+                            .FirstOrDefault(Function(r) r.Checked = True)
+                    CloseForm = rButton.Name.Substring(9)  '截取单选按钮的编号
+                    LocAL = LocALCheck.Checked
+                    LocGame = LocGameCheck.Checked
+                    My.Settings.CloseForm = CloseForm '保存最小化的操作
+                    My.Settings.LocAL = LocAL '保存是否汉化启动战网
+                    My.Settings.LocGame = LocGame '保存是否汉化启动游戏
+                    My.Settings.Save()  '保存所有设置
+                Catch ex As Exception
+                    MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Save CloseFormAction Error")
+                End Try
+                SettingPanel.Visible = False
+                OpenSettings.Text = "打开设置"
+                PanelVisible = False
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Settings Error")
+        End Try
+    End Sub
+
+    Private Sub WavenLauncher_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Try
+            My.Settings.Save()    '关闭窗体前保存用户设置
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Form Closing Error")
+        End Try
+    End Sub
+
+    Private Sub SysTrayIcon_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles SysTrayIcon.MouseDoubleClick
+        Show()
+        Focus()
+        WindowState = FormWindowState.Normal
+        SysTrayIcon.Visible = False
+    End Sub
 End Class
